cc0f866d6cd9665f9df91372f1c7e15b234600b2
Damian <damkow@gmail.com>
2017-10-17 10:36:38 +0200

    StockTicker

---
+++ SignaRChatMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignaRChatMVC.MvcApplication" Language="C#" %>
---
+++ SignalRStockTicker/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRStockTicker.Startup))]
+
+namespace SignalRStockTicker
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
